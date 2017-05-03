6536441c00cd00958da3e1393ec35d2c4a3a10bc
zyy <zyy7259@gmail.com>
2017-05-03 10:11:09 +0800

    .

--- init.sh
+++ init.sh
@@ -2,12 +2,14 @@ brew install curl wget
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-# install on-my-zsh
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# install on-my-zsh theme powerlevel9k
+# install oh-my-zsh theme powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-# install on-my-zsh plugin autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# install oh-my-zsh plugin zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# install oh-my-zsh plugin zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # make symbolic links
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
@@ -15,7 +17,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 nvm install node
 npm i -g http-server
 # brew
-brew install yarn mpv cloc autojump
+brew install yarn cloc autojump
 # cask
 brew tap caskroom/cask
-brew cask install google-chrome visual-studio-code iterm2 sourcetree gas-mask dash diffmerge smcfancontrol xee the-unarchiver wireshark synergy cheatsheet xmind neteasemusic adobe-photoshop-cc sketch microsoft-office parallels-desktop
+brew cask install 1password mpv google-chrome visual-studio-code iterm2 sourcetree gas-mask dash diffmerge smcfancontrol xee the-unarchiver wireshark synergy cheatsheet xmind neteasemusic adobe-photoshop-cc sketch microsoft-office parallels-desktop
