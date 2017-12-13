32959b618c95f97578216a3f933a00dfacc1fdb5
zyy <zyy7259@gmail.com>
2017-12-13 10:20:14 +0800

    feat: git.sh

--- git.sh
+++ git.sh
@@ -1,2 +1,3 @@
 # https://stackoverflow.com/questions/13846300/how-to-make-git-pull-use-rebase-by-default-for-all-my-repositories
+# When preserve, also pass --preserve-merges along to git rebase so that locally committed merge commits will not be flattened by running git pull.
 git config --global pull.rebase preserve
