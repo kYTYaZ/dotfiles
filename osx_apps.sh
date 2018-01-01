4623d1a76978c20822dfa46050a8e99eb7d7f74b
Yinyga Zhang <zyy7259@gmail.com>
2018-01-01 21:12:13 +0800

    feat: add vim

--- osx_apps.sh
+++ osx_apps.sh
@@ -3,4 +3,4 @@ sudo spctl --master-disable
 
 # cask
 brew tap caskroom/cask
-brew cask install xee the-unarchiver google-chrome visual-studio-code iterm2 sourcetree gas-mask dash postman java diffmerge smcfancontrol 1password wireshark synergy cheatsheet xmind neteasemusic adobe-photoshop-cc sketch microsoft-office parallels-desktop
+brew cask install vim xee the-unarchiver google-chrome visual-studio-code iterm2 sourcetree gas-mask dash postman java diffmerge smcfancontrol 1password wireshark synergy cheatsheet xmind neteasemusic adobe-photoshop-cc sketch microsoft-office parallels-desktop
