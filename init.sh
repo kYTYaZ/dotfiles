a21eb20cb7a524f0c628db0f0bdd8cc09cee4917
zyy <zyy7259@gmail.com>
2018-06-05 10:27:08 +0800

    feat: add fonts

--- init.sh
+++ init.sh
@@ -98,3 +98,25 @@ yarn config set -g registry https://registry.npm.taobao.org
 
 sh ./git.sh
 sh ./git-personal.sh
+
+###
+### fonts start
+###
+
+# powerline
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+# nerd font
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
+###
+### fonts end
+###
